f0476ca45d19a182af2b6c39db884ea146a1a403
Henri Koskinen <hkoskinen@protonmail.com>
2019-02-27 13:18:06 +0200

    Read city name from request and use it to make a request to OpenWeatherMap

--- src/main/java/com/gravenium/temperature/controller/TemperatureController.java
+++ src/main/java/com/gravenium/temperature/controller/TemperatureController.java
@@ -1,19 +1,55 @@
 package com.gravenium.temperature.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/api")
 public class TemperatureController {
 
-	@GetMapping("/temperatures")
-	public String getTemperatures() {
-		return "List of all temperaturs";
-	}
+	@Value("${app.weather.api.key}")
+	private String API_KEY;
 	
-	@GetMapping("/temperatures/{id}")
-	public String getTemperature(@PathVariable("id") long id) {
-		return "Temperature for a city with id " + id;
+	private RestTemplate rest = new RestTemplate();
+
+	@GetMapping(value = "/temperatures", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String getTemperatureByCity(@RequestParam(required = false) String city) {
+		if (city == null)
+			return "Invalid city name";
+		return makeRequest(city);
+	}
+
+	private String makeRequest(String city) {
+		String result;
+		try {
+			result = rest.getForObject("https://api.openweathermap.org/data/2.5/weather?q=" + city
+					+ "&units=metric&appid=" + API_KEY, String.class);
+		} catch (HttpClientErrorException e) {
+			ApiError er = new ApiError("Invalid city name");
+			result = er.getMessage();
+		}
+		return result;
+	}
+}
+
+class ApiError {
+	private String message;
+
+	public ApiError(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 }
