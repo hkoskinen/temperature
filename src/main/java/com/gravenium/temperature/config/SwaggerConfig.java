df516341aa61746de8b2a510f97d8cb29734530c
Henri Koskinen <hkoskinen@protonmail.com>
2019-02-27 16:15:52 +0200

    Add metadata for Swagger documentation

--- src/main/java/com/gravenium/temperature/config/SwaggerConfig.java
+++ src/main/java/com/gravenium/temperature/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.gravenium.temperature.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,14 +16,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	@Value("${app.version}")
+	private String appVersion;
 
 	@Bean
 	public Docket apiDocket() {
-		Docket docket = new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.gravenium.temperature"))
-				.paths(PathSelectors.any())
-				.build();
+		Docket docket = new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.gravenium.temperature.controller"))
+				.paths(PathSelectors.any()).build().apiInfo(metaData());
 		return docket;
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder()
+				.title("Temperature REST API")
+				.description("Simple REST API that gives you current temperature for given city name or city id.")
+				.license("MIT")
+				.licenseUrl("https://github.com/hkoskinen/temperature/blob/master/LICENSE.md")
+				.termsOfServiceUrl("")
+				.contact(new Contact("Henri Koskinen", "https://github.com/hkoskinen", "hkoskinen@protonmail.com"))
+				.version(appVersion)
+				.build();
 	}
 }
