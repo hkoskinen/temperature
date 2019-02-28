68ad818e625bb61e6503976dd64cd8642f722f58
Henri Koskinen <hkoskinen@protonmail.com>
2019-02-28 15:49:59 +0200

    Add produces attribute to annotation

--- src/main/java/com/gravenium/temperature/controller/TemperatureController.java
+++ src/main/java/com/gravenium/temperature/controller/TemperatureController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class TemperatureController {
 	@Autowired
 	private TemperatureService temperatureService;
 
-	@GetMapping(value = "/temperatures")
+	@GetMapping(value = "/temperatures", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<Response> getTemperatureByCity(@RequestParam(required = false) String city) {
 		
 		if (city == null || city.length() == 0) {
